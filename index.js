5953e5ed6eff84eb22426e401389ca5bdae7449d
Devin Norgarb <dnorgarb@gmail.com>
2020-09-13 23:19:28 +0200

    wip

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
-const path = require('path');
-const config = require('./config');
-const fastify = require('fastify')();
+const path = require("path");
+const config = require("./config");
+const fastify = require("fastify")();
 
 // compression - add x-protobuf
-fastify.register(
-  require('fastify-compress'), {
-    customTypes: /^text\/|\+json$|\+text$|\+xml|x-protobuf$/
-  }
-);
+fastify.register(require("fastify-compress"), {
+  customTypes: /^text\/|\+json$|\+text$|\+xml|x-protobuf$/,
+});
 
 // CORS
-fastify.register(require('fastify-cors'));
+fastify.register(require("fastify-cors"));
 
 // swagger documentation
-fastify.register(require('fastify-swagger'), {
+fastify.register(require("fastify-swagger"), {
   exposeRoute: true,
-  swagger: config.swagger
+  swagger: config.swagger,
 });
 
-fastify.get('/map', {schema: {hide: true}}, (req, reply) => {
-  reply.sendFile('ol.html');
+fastify.get("/map", { schema: { hide: false } }, (req, reply) => {
+  reply.sendFile("ol.html");
 });
 
-fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, 'public'),
-  prefix: '/',
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/",
 });
 
 // routes
-fastify.register(require('fastify-autoload'), {
-  dir: path.join(__dirname, 'routes')
+fastify.register(require("fastify-autoload"), {
+  dir: path.join(__dirname, "routes"),
 });
 
 // Launch server
-fastify.listen(config.server.port, (config.server.host || 'localhost'), function (err, address) {
+fastify.listen(config.server.port, config.server.host || "localhost", function (
+  err,
+  address
+) {
   if (err) {
     console.log(err);
     process.exit(1);
--- routes/geojson.js
+++ routes/geojson.js
@@ -2,11 +2,11 @@ const config = require("../config");
 const Tile38 = require("tile38");
 const Tile38client = new Tile38(config.tile38);
 
+// const
 const GeoJSON = require("geojson");
 const geojsonvt = require("geojson-vt");
 const vtpbf = require("vt-pbf");
 
-// route schema
 const schema = {
   description: "Return table as VT",
   tags: ["feature"],
@@ -35,46 +35,44 @@ const schema = {
 module.exports = function (fastify, opts, next) {
   fastify.route({
     method: "GET",
-    url: "/intersects/:table/:z/:x/:y",
+    url: "/geojson/:table/:z/:x/:y",
     schema: schema,
     handler: function (request, reply) {
-      console.error(
-        request.params.table,
-        "/" + request.params.z + "/" + request.params.x + "/" + request.params.y
-      );
+      // console.error(request.params.table, "/" + 12 + "/" + 12 + "/" + 12);
       let intersects = Tile38client.intersectsQuery(request.params.table)
+        .cursor(100)
         .tile(request.params.x, request.params.y, request.params.z)
-        .cursor(1000)
-        .limit(1000000);
+        .limit(10000);
       intersects
         .execute()
         .then((results) => {
           if (results.count && results.count > 0) {
             const res = GeoJSON.parse(results.objects, { GeoJSON: "object" });
+            // console.log(res);
 
-            var tileOptions = {
-              maxZoom: 24,
-              minZoom: 1,
-              tolerance: 4,
-              extent: 4096, // tile extent (both width and height)
-              buffer: 64,
-              debug: 0,
-              indexMaxZoom: 24,
-              indexMinZoom: 1,
-              indexMaxPoints: 1000000,
-              solidChildren: false, // whether to include solid tile children in the index
-            };
+            reply.type("application/json").send(res);
 
-            const tileIndex = geojsonvt(res, tileOptions);
-            const tile = tileIndex.getTile(
-              request.params.z,
-              request.params.x,
-              request.params.y
-            );
-            const buffer = Buffer.from(
-              vtpbf.fromGeojsonVt({ geojsonLayer: tile })
-            );
-            reply.type("application/protobuf").send(buffer);
+            // var tileOptions = {
+            //   maxZoom: 24,
+            //   tolerance: 7,
+            //   extent: 4096, // tile extent (both width and height)
+            //   buffer: 64,
+            //   debug: 2,
+            //   indexMaxZoom: 24,
+            //   indexMaxPoints: 10000,
+            //   solidChildren: false, // whether to include solid tile children in the index
+            // };
+
+            // const tileIndex = geojsonvt(res, tileOptions);
+            // const tile = tileIndex.getTile(
+            //   request.params.z,
+            //   request.params.x,
+            //   request.params.y
+            // );
+            // const buffer = Buffer.from(
+            //   vtpbf.fromGeojsonVt({ geojsonLayer: tile })
+            // );
+            // reply.type("application/protobuf").send(buffer);
           } else {
             reply.type("application/protobuf").send();
           }
